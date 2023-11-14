70f8f42d5a7073ad91c788af90f7bcf9e2fd92f1
Maria Stoelben <maria-suanne.stoelben@hec.edu>
2023-11-14 16:09:07 +0000

    added folds and columns that are used to drop NAs

--- config/config_data.py
+++ config/config_data.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 
+# data load
 DATA_DIR = (
     Path("..") / "hfactory_magic_folders" / "plastic_cost_prediction" / "data"
 )
 MAIN_FILE = "PA6_cleaned_dataset.csv"
+
+# data preprocessing
+DROPNA_COLS = ["best_price_compound"]
+N_FOLDS = 10
