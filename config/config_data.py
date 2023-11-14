fe2739149cccd5ff7dd83e7f114a290df333e59a
Maria Stoelben <maria-suanne.stoelben@hec.edu>
2023-11-14 15:12:00 +0000

    added main file to config

--- config/config_data.py
+++ config/config_data.py
@@ -1,3 +1,6 @@
 from pathlib import Path
 
-DATA_DIR = Path("..") / "hfactory_magic_folders" / "plastic_cost_prediction" / "data"
+DATA_DIR = (
+    Path("..") / "hfactory_magic_folders" / "plastic_cost_prediction" / "data"
+)
+MAIN_FILE = "PA6_cleaned_dataset.csv"
