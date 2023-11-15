be4378641de603c529cce8182be5aa50b6976de3
Maria Stoelben <maria-suanne.stoelben@hec.edu>
2023-11-15 18:21:52 +0000

    init code for univariate modeling

---
+++ config/config_modeling.py
@@ -0,0 +1,3 @@
+P_RANGE = {"best_price_compound": list(range(0, 4))}
+Q_RANGE = {"best_price_compound": list(range(0, 4))}
+D = {"best_price_compound": 1}
---
+++ src/modeling/univariate_modeling.py
@@ -0,0 +1,84 @@
+from typing import List, Tuple
+
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+
+from config.config_modeling import P_RANGE, Q_RANGE, D
+from src.data_preprocessing.data_pipeline import data_pipeline
+
+
+def grid_search_arima(
+    ts: pd.Series, p_values: List[int], d: int, q_values: List[int]
+) -> Tuple[int, int, int]:
+    """Performs a grid search for ARIMA based on AIC.
+
+    Parameters
+    ----------
+    ts : pd.Series
+        tbd
+    p_values : List[int]
+        List of candidate values for p.
+    d : int
+        d value for difference to make series stationary.
+    q_values : List[int]
+        List of candidate values for q.
+
+    Returns
+    -------
+    best_order : Tuple[int, int, int]
+        Best ARIMA model order (p, d, q)
+    """
+    best_aic = float("inf")
+    best_order = None
+
+    for p in p_values:
+        for q in q_values:
+            order = (p, d, q)
+            model = ARIMA(ts, order=order)
+            model_fit = model.fit()
+            aic = model_fit.aic
+
+            if aic < best_aic:
+                best_aic = aic
+                best_order = order
+
+    return best_order
+
+
+def get_arima_model(df: pd.DataFrame) -> dict[str, ARIMA]:
+    """Get arima model for each column of df.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        tbd
+
+    Returns
+    -------
+    models : dict[str, ARIMA]
+        Dict of univariate models for each column.
+    """
+    models = {}
+    for col in df.columns:
+        if col not in Q_RANGE.keys():
+            continue
+
+        series = df[col]
+        series.index = df.time
+        series.index = series.index.to_period("M")
+
+        best_order = grid_search_arima(
+            series, P_RANGE[col], D[col], Q_RANGE[col]
+        )
+        print(f"{col} - Best ARIMA Order: {best_order}")
+
+        model = ARIMA(series, order=best_order)
+        model_fit = model.fit()
+        models[col] = model_fit
+
+    return models
+
+
+if __name__ == "__main__":
+    df, _ = data_pipeline()
+    get_arima_model(df)
