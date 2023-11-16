66d2086615b4714e3256efbfae36a867e75deeb1
Maria Stoelben <maria-suanne.stoelben@hec.edu>
2023-11-16 15:42:05 +0000

    added seasonal terms in ARIMA and add non stationary terms

--- src/modeling/univariate_modeling.py
+++ src/modeling/univariate_modeling.py
@@ -4,14 +4,18 @@ from typing import List, Tuple
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 
-from config.config_modeling import P_RANGE, Q_RANGE, D
+from config.config_modeling import P_RANGE, Q_RANGE, SEASONAL_TERMS, D
 from src.data_preprocessing.data_pipeline import data_pipeline
 
 warnings.filterwarnings("ignore")
 
 
 def grid_search_arima(
-    ts: pd.Series, p_values: List[int], d: int, q_values: List[int]
+    ts: pd.Series,
+    p_values: List[int],
+    d: int,
+    q_values: List[int],
+    seasonal: Tuple[int] = (0, 0, 0, 0),
 ) -> Tuple[Tuple[int, int, int], int]:
     """Performs a grid search for ARIMA based on AIC.
 
@@ -25,6 +29,8 @@ def grid_search_arima(
         d value for difference to make series stationary.
     q_values : List[int]
         List of candidate values for q.
+    seasonal : Tuple[int]
+        Tuple for seasonal ARIMA.
 
     Returns
     -------
@@ -39,7 +45,7 @@ def grid_search_arima(
     for p in p_values:
         for q in q_values:
             order = (p, d, q)
-            model = ARIMA(ts, order=order)
+            model = ARIMA(ts, order=order, seasonal_order=seasonal)
             model_fit = model.fit()
             aic = model_fit.aic
 
@@ -73,10 +79,17 @@ def get_arima_model(df: pd.DataFrame) -> dict[str, ARIMA]:
         series.index = series.index.to_period("M")
 
         best_order, best_aic = grid_search_arima(
-            series, P_RANGE[col], D[col], Q_RANGE[col]
+            series,
+            P_RANGE[col],
+            D[col],
+            Q_RANGE[col],
+            SEASONAL_TERMS.get(col, (0, 0, 0, 0)),
         )
         print(f"{col}")
-        print(f"- Best ARIMA Order: {best_order}")
+        print(
+            f"- Best ARIMA Order: {best_order} "
+            f"x {SEASONAL_TERMS.get(col, (0, 0, 0, 0))}"
+        )
         print(f"- AIC: {best_aic:.2f}")
         print("---------------")
 
