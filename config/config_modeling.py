9a040026b431557de0f2f34246fea89e2933f38f
Maria Stoelben <maria-suanne.stoelben@hec.edu>
2023-11-16 14:21:45 +0000

    add params for stationary series

--- config/config_modeling.py
+++ config/config_modeling.py
@@ -1,3 +1,24 @@
-P_RANGE = {"best_price_compound": list(range(0, 4))}
-Q_RANGE = {"best_price_compound": list(range(0, 4))}
-D = {"best_price_compound": 1}
+P_RANGE = {
+    "best_price_compound": list(range(0, 4)),
+    "Electricty_Price_Netherlands": list(range(0, 4)),
+    "Electricty_Price_France": list(range(0, 4)),
+    "Electricty_Price_Italy": list(range(0, 4)),
+    "NGAS_EUR": list(range(0, 2)),
+    "iNATGAS": list(range(0, 4)),
+}
+Q_RANGE = {
+    "best_price_compound": list(range(0, 4)),
+    "Electricty_Price_Netherlands": list(range(0, 6)),
+    "Electricty_Price_France": list(range(0, 6)),
+    "Electricty_Price_Italy": list(range(0, 6)),
+    "NGAS_EUR": list(range(0, 6)),
+    "iNATGAS": list(range(0, 4)),
+}
+D = {
+    "best_price_compound": 1,
+    "Electricty_Price_Netherlands": 0,
+    "Electricty_Price_France": 0,
+    "Electricty_Price_Italy": 0,
+    "NGAS_EUR": 0,
+    "iNATGAS": 0,
+}
