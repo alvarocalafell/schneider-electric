09c0a6deb90a28991c0f0e6cfd8e71e8869ee360
Maria Stoelben <maria-suanne.stoelben@hec.edu>
2023-11-17 09:37:51 +0000

    moved config content to data

--- config/config_data.py
+++ config/config_data.py
@@ -1,3 +1,30 @@
 from pathlib import Path
 
-DATA_DIR = Path("..") / "hfactory_magic_folders" / "plastic_cost_prediction" / "data"
+DATA_DIR = (
+    Path("..") / "hfactory_magic_folders" / "plastic_cost_prediction" / "data"
+)
+
+GROUPING_VARS = [
+    ["CRUDE_PETRO", "CRUDE_BRENT", "CRUDE_DUBAI", "CRUDE_WTI"],
+    [
+        "Electricty_Price_France",
+        "Electricty_Price_Italy",
+        "Electricty_Price_Poland",
+        "Electricty_Price_Netherlands",
+        "Electricty_Price_Germany",
+    ],
+    ["NGAS_US", "NGAS_EUR", "NGAS_JP"],
+]
+
+GROUPING_NAMES = ["CRUDE_AVG", "Electricity_AVG", "NGAS_AVG"]
+
+GROUPING_FUNCS = ["mean", "mean", "mean"]
+
+TIME_VARS = ["year", "month", "quarter"]
+
+DROP_VARS = [
+    "PA6 GLOBAL_ EMEAS _ EUR per TON",
+    "Benzene_price",
+    "Cyclohexane_price",
+    "iNATGAS",
+]
