a15bea885cc42737a77ae417d8b9207218069356
Maria Stoelben <maria.stlbn@hotmail.de>
2023-11-23 12:19:34 +0100

    updated to final version

--- src/modeling/multivariate_modeling.py
+++ src/modeling/multivariate_modeling.py
@@ -1,27 +1,29 @@
-from typing import Union
+from typing import Tuple, Union
 
+import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.vector_ar.var_model import VAR
 
 from src.data_preprocessing.data_loader import time_split
+from src.modeling.evaluation import mae, smape
+
+TARGET = "best_price_compound"
 
 
 def adfuller_test(
     series: Union[list, tuple, pd.Series], sig: float = 0.05, name: str = ""
 ) -> None:
-    """
+    """Perform Augmented Dickey-Fuller test to check stationarity.
 
-    Perform Augmented Dickey-Fuller test to check stationarity of a time
-    series.
-
-    Parameters:
+    Parameters
+    ----------
     - series (array-like): The time series data to be tested for stationarity.
     - sig (float): The significance level for the test. Default is 0.05.
     - name (str): A label or name for the series (optional).
 
-    Returns:
+    Returns
+    -------
     None: Prints the result of the Augmented Dickey-Fuller test.
     """
     res = adfuller(series, autolag="AIC")
@@ -33,144 +35,69 @@ def adfuller_test(
         print(f" {name} : P-Value = {p_value} => Non-stationary.")
 
 
-def invert_transformation(
-    df_train: pd.DataFrame, df_forecast: pd.DataFrame, third_diff: bool
-) -> pd.DataFrame:
+def grid_search_var(
+    ts: pd.Series,
+) -> Tuple[int, float, float, VAR, np.ndarray[float]]:
+    """Performs a grid search for VAR based on cv sMAPE.
 
+    Parameters
+    ----------
+    ts : pd.Series
+        Time series data.
+
+    Returns
+    -------
+    best_order : int
+        Best VAR model order.
+    best_sMAPE : float
+        Avg. sMAPE for best VAR model.
+    best_MAE : float
+        Avg. MAE for best VAR model.
+    best_model : VAR
+        Best VAR model trained on final fold.
+    best_preds : np.ndarray[float]
+        Predictions of best model of last fold.
     """
-    Inverts differencing transformations applied during time series
-    forecasting.
+    best_sMAPE = float("inf")
+    spl = time_split(ts)
 
-    Parameters:
-    - df_train (DataFrame): The training DataFrame containing the original
-    time series data.
-    - df_forecast (DataFrame): The DataFrame containing the forecasted values
-    after differencing.
-    - third_diff (bool): If True, it assumes that a third-order differencing
-    was applied.
-
-    Returns:
-    - DataFrame: A DataFrame containing the inverted forecasted values.
-    """
-    df_fc = df_forecast.copy()
-    columns = df_train.columns
-
-    for col in columns:
-        if third_diff is True:
-            # Roll back 3rd Diff
-            df_fc[str(col) + "_2d"] = (
-                df_train[col].iloc[-1]
-                - df_train[col].iloc[-2]
-                + df_fc[str(col) + "_3d"].cumsum()
-            )
-            df_fc[str(col) + "_1d"] = (
-                df_train[col].iloc[-2]
-                - df_train[col].iloc[-3]
-                + df_fc[str(col) + "_2d"].cumsum()
-            )
-            df_fc[str(col) + "_forecast"] = (
-                df_train[col].iloc[-3] + df_fc[str(col) + "_1d"].cumsum()
-            )
-
-        else:
-            # Roll back 2nd Diff
-            df_fc[str(col) + "_1d"] = (
-                df_train[col].iloc[-1] - df_train[col].iloc[-2]
-            ) + df_fc[str(col) + "_2d"].cumsum()
-            df_fc[str(col) + "_forecast"] = (
-                df_train[col].iloc[-1] + df_fc[str(col) + "_1d"].cumsum()
-            )
-    return df_fc
-
-
-def grid_search_var(df: pd.DataFrame):
-
-    """
-    Find the VAR model with the highest AIC.
-
-    Parameters:
-    - df: pandas DataFrame, the input DataFrame with time series data.
-
-    Returns:
-    - best_model: statsmodels.tsa.vector_ar.var_model.VARResultsWrapper,
-    the best VAR model.
-    - best_order: int, the order of the best VAR model.
-    """
-    best_aic = float("inf")
-    best_model = None
-    best_order = None
-
+    # iterate over possible orders
     for order in range(1, 10):
-        model = VAR(df)
-        results = model.fit(order)
-        aic = results.aic
-        print(f"Order = {order}, AIC = {aic}")
+        sMAPE_total, MAE_total = 0.0, 0.0
 
-        if aic < best_aic:
-            best_aic = aic
-            best_model = results
-            best_order = order
-
-    print(f"\nBest Model Order = {best_order}")
-    print(f"Best Model AIC = {best_aic}")
-
-    return best_model, best_order
-
-    def get_var_model(
-        df: pd.DataFrame, df_stationary: pd.DataFrame, third_diff=False
-    ):
-
-        """
-        Fit a Vector Autoregressive (VAR) model on a stationary time
-        series and generate forecasts.
-
-        Parameters:
-        - df (DataFrame): The original time series DataFrame.
-        - df_stationary (DataFrame): The DataFrame containing the
-        stationary time series data.
-        - third_diff (bool): If True, it assumes that a third-order
-        differencing was applied.
-
-        Returns:
-        - DataFrame: A DataFrame containing the forecasted values.
-        """
-        df_stationary.dropna(inplace=True)
-        spl = time_split(df_stationary)
-
+        # iterate over cv folds
         for train_idx, test_idx in spl:
-            train = df_stationary.iloc[train_idx]
-            test = df_stationary.iloc[test_idx]
+            train = ts.iloc[train_idx]
+            test = ts.iloc[test_idx]
 
-        best_model, best_order = grid_search_var(train)
-        print(best_model.summary())
+            # train model for fold and order
+            model = VAR(train)
+            model_fit = model.fit(order)
 
-        lag_order = best_model.k_ar
-        forecast_input = df_stationary.values[-lag_order:]
-
-        nobs = len(train)
-        fc = best_model.forecast(y=forecast_input, steps=nobs)
-        if third_diff is True:
-            df_forecast = pd.DataFrame(
-                fc, index=df.index[-nobs:], columns=df.columns + "_3d"
+            # predictions for fold
+            lag_order = model_fit.k_ar
+            forecast_input = test.to_numpy()[-lag_order:]
+            pred_values = model_fit.forecast(y=forecast_input, steps=len(test))
+            preds = pd.DataFrame(
+                pred_values, index=test.index, columns=test.columns
             )
-        else:
-            df_forecast = pd.DataFrame(
-                fc, index=df.index[-nobs:], columns=df.columns + "_2d"
-            )
-        df_results = invert_transformation(train, df_forecast, df)
 
-        fig, axes = plt.subplots(
-            nrows=int(len(df.columns) / 2), ncols=2, dpi=150, figsize=(20, 20)
-        )
-        for i, (col, ax) in enumerate(zip(df.columns, axes.flatten())):
-            df_results[col + "_forecast"].plot(legend=True, ax=ax).autoscale(
-                axis="x", tight=True
-            )
-            test[col][-nobs:].plot(legend=True, ax=ax)
-            ax.set_title(col + ": Forecast vs Actuals")
-            ax.xaxis.set_ticks_position("none")
-            ax.yaxis.set_ticks_position("none")
-            ax.spines["top"].set_alpha(0)
-            ax.tick_params(labelsize=6)
+            # get sMAPE and MAE over 3,6,9 months on test for fold
+            sMAPE = smape(test[TARGET][2::3], preds[TARGET][2::3])
+            MAE = mae(test[2::3], preds[2::3])
+            sMAPE_total += sMAPE
+            MAE_total += MAE
 
-        plt.tight_layout()
+        # average MAE and sMAPE over folds
+        avg_sMAPE = sMAPE_total / len(spl)
+        avg_MAE = MAE_total / len(spl)
+
+        # assign best order and add. information if sMAPE is smaller
+        if avg_sMAPE < best_sMAPE:
+            best_sMAPE = avg_sMAPE
+            best_MAE = avg_MAE
+            best_order = order
+            best_model = model_fit
+            best_preds = preds
+
+    return best_order, best_sMAPE, best_MAE, best_model, best_preds
